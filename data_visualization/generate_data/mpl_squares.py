b40759de44d9b72239b3513bc542c69d808f0ccc
xiaoxiaopingzi <1641084175@qq.com>
2017-08-11 21:54:27 +0800

    第二个项目：数据可视化

--- data_visualization/generate_data/mpl_squares.py
+++ data_visualization/generate_data/mpl_squares.py
@@ -1,3 +1,15 @@
 # -*- coding: UTF-8 -*-
-"""  """
+"""使用matplotlib绘制一个简单的折线图"""
+import matplotlib.pyplot as plt
 
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares, linewidth=2)
+
+# 设置图标标题，并给坐标轴加上标签
+plt.title("Square Number", fontsize=16)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis="both", labelsize=14)
+plt.show()
