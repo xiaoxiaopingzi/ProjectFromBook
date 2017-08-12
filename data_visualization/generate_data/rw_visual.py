3abd449ace6040d1ac96c04b2b9fa7b8c14dc446
xiaoxiaopingzi <1641084175@qq.com>
2017-08-12 14:51:41 +0800

    设置随机漫步图的样式

--- data_visualization/generate_data/rw_visual.py
+++ data_visualization/generate_data/rw_visual.py
@@ -8,9 +8,21 @@ except ImportError:
     raise ImportError('The file is not found. Please check the file name!')
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    # 设置绘图窗口的尺寸
+    plt.figure(figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor="none", s=1)
+    plt.scatter(0, 0, c="green", edgecolors="none", s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
+
+    # 隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n)")
